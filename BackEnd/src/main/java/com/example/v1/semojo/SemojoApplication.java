e0c06aebf6d61885219f8c709830af86611ded1b
JasonAchillious <11711621@mail.sustech.edu.cn>
2020-09-19 01:18:32 +0800

    config the env of backend
    add springboot framwork and nothing else.

---
+++ BackEnd/src/main/java/com/example/v1/semojo/SemojoApplication.java
@@ -0,0 +1,13 @@
+package com.example.v1.semojo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SemojoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SemojoApplication.class, args);
+    }
+
+}
---
+++ BackEnd/src/test/java/com/example/v1/semojo/SemojoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.v1.semojo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SemojoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
