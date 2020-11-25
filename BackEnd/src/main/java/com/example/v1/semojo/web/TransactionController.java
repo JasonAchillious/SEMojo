ff5f05e53b9b659eea2e9793764b9ec2455f6eb9
JasonAchillious <11711621@mail.sustech.edu.cn>
2020-11-25 10:15:19 +0800

    add create function to transaction part

--- BackEnd/src/main/java/com/example/v1/semojo/web/TransactionController.java
+++ BackEnd/src/main/java/com/example/v1/semojo/web/TransactionController.java
@@ -3,6 +3,7 @@ package com.example.v1.semojo.web;
 import com.example.v1.semojo.api.WebRespResult;
 import com.example.v1.semojo.api.model.ProductID;
 import com.example.v1.semojo.entities.Product;
+import com.example.v1.semojo.entities.Transaction;
 import com.example.v1.semojo.services.TransactionService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +13,32 @@ public class TransactionController {
     @Autowired
     TransactionService transacService;
 
-    @PostMapping("/customer/{username}/transaction")
-    public WebRespResult crea(@PathVariable String username,
+    @PostMapping("/customer/{username}/shoppingcart/transaction")
+    public WebRespResult createTransaction(@PathVariable String username,
                               @RequestParam ProductID products){
-        return null;
+        try{
+            Transaction transac = transacService.createTransaction(products.getProjectIds(), username);
+            return new WebRespResult<>(200, "success", transac);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new WebRespResult(400, e.getMessage());
+        }
     }
 
-    @PutMapping("/customer/{username}/transaction/{transaction")
+    @PostMapping("/customer/{username}/product/{productId}/transaction")
+    public WebRespResult creatTransac(@PathVariable String username, @PathVariable Long productId){
+        try{
+            Transaction transac = transacService.createTransaction(productId, username);
+            return new WebRespResult<>(200, "success", transac);
+        }catch (Exception e){
+            e.printStackTrace();
+            return new WebRespResult(400, e.getMessage());
+        }
+    }
+
+    @PutMapping("/customer/{username}/transaction/{transactionId}")
     public WebRespResult changeTransactionStatus(@PathVariable String username,
-                                                 @PathVariable Long projectId,
+                                                 @PathVariable Long transactionId,
                                                  @RequestParam String status){
         return null;
     }
