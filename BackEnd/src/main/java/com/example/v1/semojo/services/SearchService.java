691d2cfb4212e7290adb6c0f90491be644e76628
JasonAchillious <11711621@mail.sustech.edu.cn>
2020-12-24 05:20:44 +0800

    search user

--- BackEnd/src/main/java/com/example/v1/semojo/services/SearchService.java
+++ BackEnd/src/main/java/com/example/v1/semojo/services/SearchService.java
@@ -1,8 +1,43 @@
 package com.example.v1.semojo.services;
 
+import com.example.v1.semojo.api.model.UserAllInfoModel;
+import com.example.v1.semojo.api.model.UserInfoModel;
+import com.example.v1.semojo.dao.ProductDao;
+import com.example.v1.semojo.dao.UserAuthDao;
+import com.example.v1.semojo.entities.User;
+import com.example.v1.semojo.entities.UserAuth;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class SearchService {
+    @Autowired
+    UserAuthDao userAuthDao;
+    @Autowired
+    ProductDao productDao;
 
+    public List<UserAllInfoModel> searchUserNameLike(String userName){
+        List<UserAuth> userAuthList = userAuthDao.findByUsernameLike(userName);
+        List<UserAllInfoModel> userInfoList = new ArrayList<>();
+        if (userAuthList != null){
+            for (UserAuth userAuth : userAuthList){
+                User userTemp = userAuth.getUser();
+                UserAllInfoModel n_model = new UserAllInfoModel();
+                n_model.setUsername(userAuth.getUsername());
+                n_model.setAddress(userTemp.getAddress());
+                n_model.setAuths(userAuth);
+                n_model.setEmail(userTemp.getEmail());
+                n_model.setGender(userTemp.getGender());
+                n_model.setPhoneNum(userTemp.getPhoneNum());
+                n_model.setPortrait(userTemp.getPortrait());
+                n_model.setQqNum(userTemp.getQqNum());
+                n_model.setWeChatNum(userTemp.getWeChatNum());
+                userInfoList.add(n_model);
+            }
+        }
+        return userInfoList;
+    }
 }
--- BackEnd/src/main/java/com/example/v1/semojo/web/SearchController.java
+++ BackEnd/src/main/java/com/example/v1/semojo/web/SearchController.java
@@ -1,4 +1,39 @@
 package com.example.v1.semojo.web;
 
+import com.example.v1.semojo.api.WebRespResult;
+import com.example.v1.semojo.api.model.UserAllInfoModel;
+import com.example.v1.semojo.services.SearchService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RestController
 public class SearchController {
+    @Autowired
+    SearchService searchService;
+    @Resource
+    private MongoTemplate mongoTemplate;
+
+    @GetMapping("/search/user")
+    public WebRespResult searchUserByName(@RequestParam String username){
+        List<UserAllInfoModel> userList = searchService.searchUserNameLike(username);
+        return new WebRespResult<List>(200, "success", userList);
+    }
+
+    @GetMapping("search/product")
+    public WebRespResult searchProduct(@RequestParam String keyword){
+
+        return new WebRespResult(200, "success");
+    }
+
+    @GetMapping("search/code")
+    public WebRespResult searchCode(@RequestParam String keyword){
+
+        return new WebRespResult(200, "success");
+    }
 }
